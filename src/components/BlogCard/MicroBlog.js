426b359f9adc9c167275ee0d5d7ba6c255b95031
DivyaKumarBaid <divyakumarbaid.dkb.2001@gmail.com>
2022-11-24 19:58:01 +0530

    was calling div twice

--- src/components/BlogCard/MicroBlog.js
+++ src/components/BlogCard/MicroBlog.js
@@ -4,7 +4,7 @@ import { coldarkDark as dark } from 'react-syntax-highlighter/dist/cjs/styles/pr
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 import React from 'react'
-import { AiFillHeart} from 'react-icons/ai'
+import { AiFillHeart } from 'react-icons/ai'
 
 
 export function MicroBlog(props) {
@@ -13,109 +13,108 @@ export function MicroBlog(props) {
   const [isPending, setPending] = React.useState(true)
   const [error, setError] = React.useState(null)
 
-  React.useEffect(() => { 
+  console.log(data)
+
+  React.useEffect(() => {
     fetch(`https://devblogs2022.herokuapp.com/blog/?limit=10`)
       .then(res => {
-          if (!res === 200 || !res === 201) {
-              throw Error('Could not fetch the data form the servre');
-          }
-          return res.json();
+        if (!res === 200 || !res === 201) {
+          throw Error('Could not fetch the data form the servre');
+        }
+        return res.json();
       })
       .then(data => {
-          setData(data);
-          setPending(false);
-          setError(null);
+        setData(data);
+        setPending(false);
+        setError(null);
       })
       .catch(err => {
-          if (err.name !== 'AbortError') {
-              setPending(false);
-              setError(err.message);
-          }
+        if (err.name !== 'AbortError') {
+          setPending(false);
+          setError(err.message);
+        }
       })
-  },[])
-  
-    const mode = props.mode ? "" : "dark"
- 
-    const error_div = error && <h2 className="Loading">{error}</h2>
-    const loading_div = isPending && <h2 className="Loading">Loading...</h2>
-    const microblog_div = (!isPending && data != null) ? data.map(blog => {
-        return micro(blog)
-    }) : ''
-  
-  
-    function micro(data) {
-        let likes = (data.liked_by).length
-        console.log(likes)
-        return (
-            <Link href={`/blog/${data.id}`} className="container Micro">
-                <div className={`MicroBlog  ${mode}`}>
-                    <div className="content-container">
-                        <div className='MicroBlog-top'>
-                            <h5 className='Name'>{data.author}</h5>
-                            <h6 className='Date'>{data.date}</h6>
-                        </div>
-                        <div className="microarticle">
-                            <h2 className='MicroBlog-title'>{data.title}</h2>
-                            <div className="microcontent">
-                                <article className='mircroarticle-body'>
-                                    {<Md text={data.body} />}
-                                </article>
-                            </div>
-                        </div>
-                        <div className="likes">
-                            <AiFillHeart/> {likes}
-                        </div>
-                        <div className="tags">
-                            {data.tags.map((tag,idx) => <h5 key={ idx} className="tagContent">{tag}</h5>)}
-                        </div>
-                    </div>
-                    <img src={data.image} alt="img" />
-                </div>
-            </Link >
-        )
-    }
-  
-  
-  
-    return (
-      <div className='container'>
-        {error_div}
-        {loading_div}
-        {microblog_div}
-        {microblog_div}
+  }, [])
+
+  const mode = props.mode ? "" : "dark"
+
+  const error_div = error && <h2 className="Loading">{error}</h2>
+  const loading_div = isPending && <h2 className="Loading">Loading...</h2>
+  const microblog_div = (!isPending && data != null) ? data.map(blog => {
+    return micro(blog, mode)
+  }) : ''
+
+
+  return (
+    <div className='container'>
+      {error_div}
+      {loading_div}
+      {microblog_div}
+    </div>
+  )
+}
+
+function micro(data, mode) {
+  let likes = (data.liked_by).length
+  console.log(likes)
+  return (
+    <Link href={`/blog/${data.id}`} className="container Micro">
+      <div className={`MicroBlog  ${mode}`}>
+        <div className="content-container">
+          <div className='MicroBlog-top'>
+            <h5 className='Name'>{data.author}</h5>
+            <h6 className='Date'>{data.date}</h6>
+          </div>
+          <div className="microarticle">
+            <h2 className='MicroBlog-title'>{data.title}</h2>
+            <div className="microcontent">
+              <article className='mircroarticle-body'>
+                {<Md text={data.body} />}
+              </article>
+            </div>
+          </div>
+          <div className="likes">
+            <AiFillHeart /> {likes}
+          </div>
+          <div className="tags">
+            {data.tags.map((tag, idx) => <h5 key={idx} className="tagContent">{tag}</h5>)}
+          </div>
+        </div>
+        <img src={data.image} alt="img" />
       </div>
-    )
-  }
-  
-  const Md = ({text}) => {
-    return (
-      <ReactMarkdown
-        // eslint-disable-next-line react/no-children-prop
-        children={text}
-        remarkPlugins={[remarkGfm]}
-        components={{
-          code({ node, inline, className, children, ...props }) {
-            const match = /language-(\w+)/.exec(className || '')
-            return !inline && match ? (
-              <SyntaxHighlighter
-                // eslint-disable-next-line react/no-children-prop
-                children={String(children).replace(/\n$/, '')}
-                style={dark}
-                language={match[1]}
-                PreTag="div"
-                {...props}
-              />
-            ) : (
-              <code className={className} {...props}>
-                {children}
-              </code>
-            )
-          },
-        }}
-      />
-    )
-  }
-  
+    </Link >
+  )
+}
+
+const Md = ({ text }) => {
+  return (
+    <ReactMarkdown
+      // eslint-disable-next-line react/no-children-prop
+      children={text}
+      remarkPlugins={[remarkGfm]}
+      components={{
+        code({ node, inline, className, children, ...props }) {
+          const match = /language-(\w+)/.exec(className || '')
+          return !inline && match ? (
+            <SyntaxHighlighter
+              // eslint-disable-next-line react/no-children-prop
+              children={String(children).replace(/\n$/, '')}
+              style={dark}
+              language={match[1]}
+              PreTag="div"
+              {...props}
+            />
+          ) : (
+            <code className={className} {...props}>
+              {children}
+            </code>
+          )
+        },
+      }}
+    />
+  )
+}
+
 /* <div className={`MicroBlog  ${mode}`}>
 <div className="content-container">
 
---
+++ src/fake_data/BlogData.js
@@ -0,0 +1,30 @@
+export const blogs = [
+    {
+        "id": "2181a982-8144-455f-a4ae-8258745708ec",
+        "title": "hello world",
+        "date": "2022-02-06",
+        "author": "Divya Kumar Baid",
+        "author_id": "602dcd7e-6d06-48a4-b6e4-072449371586",
+        "image": "https://wallpaperaccess.com/download/cool-52447",
+        "body": "So this is just a test dont worry we are all good  ... See more",
+        "tags": [
+            "hello",
+            "night"
+        ],
+        "liked_by": []
+    },
+    {
+        "id": "f1f0ec2e-f1f2-4de8-9d86-b92c60600cbd",
+        "title": "This is so cool",
+        "date": "2022-02-06",
+        "author": "Divya Kumar Baid",
+        "author_id": "602dcd7e-6d06-48a4-b6e4-072449371586",
+        "image": "https://media.istockphoto.com/photos/very-closeup-view-of-amazing-domestic-pet-in-mirror-round-fashion-is-picture-id1281804798?b=1&k=20&m=1281804798&s=170667a&w=0&h=HIWbeaP_cQSngCz7l9t3xwyE2eyzVgIy3K6xIqPhJQA=",
+        "body": "this is cool  ... See more",
+        "tags": ["helo ",
+            "lo",
+            "ol"
+        ],
+        "liked_by": []
+    }
+]
